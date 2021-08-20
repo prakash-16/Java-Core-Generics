3011b80cbbdeb93a8fce3ddcfd3199b54d949522
Prakash-16 <goudprakash58@gmail.com>
2021-08-21 00:31:58 +0530

    [Prakash] - Refactored the code with generic class

--- src/com/bridgelabz/JCG/genericBasedPrograms.java
+++ src/com/bridgelabz/JCG/genericBasedPrograms.java
@@ -1,23 +1,31 @@
 package com.bridgelabz.JCG;
 
-public class genericBasedPrograms {
+public class genericBasedPrograms<E extends Comparable<E>> {
+	public E a;
+	public E b;
+	public E c;
 	
-	
-	public static <E extends Comparable<E>> E maxData(E a, E b, E c) {
+	public genericBasedPrograms(E a, E b,E c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	public E maxData() {
 	      E max = a;   
-	      
+
 	      if(b.compareTo(max) > 0) {
 	         max = b;   
 	      }
-	      
+
 	      if(c.compareTo(max) > 0) {
 	         max = c;                   
 	      }
-	      return max;     
+	      return max;
 	   }
 	
 	public static void main(String[] args) {
-		System.out.println(maxData("Apple","Banana","Peaches"));
+		genericBasedPrograms<Integer> B1 = new genericBasedPrograms<Integer>(1,2,3);
+		System.out.println("The Maximum data is : " + B1.maxData());
 	}
-
+	
 }
