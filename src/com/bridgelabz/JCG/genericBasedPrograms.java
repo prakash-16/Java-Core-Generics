f6a129509205d0907cd142ba2b926841abd4fb9e
Prakash-16 <goudprakash58@gmail.com>
2021-08-21 00:36:53 +0530

    [Prakash] - Added the feature to caluclate maximum for more than 3 parameters

--- src/com/bridgelabz/JCG/genericBasedPrograms.java
+++ src/com/bridgelabz/JCG/genericBasedPrograms.java
@@ -4,11 +4,13 @@ public class genericBasedPrograms<E extends Comparable<E>> {
 	public E a;
 	public E b;
 	public E c;
+	public E d;
 	
-	public genericBasedPrograms(E a, E b,E c) {
+	public genericBasedPrograms(E a, E b,E c, E d) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
+		this.d = d;
 	}
 	public E maxData() {
 	      E max = a;   
@@ -20,11 +22,14 @@ public class genericBasedPrograms<E extends Comparable<E>> {
 	      if(c.compareTo(max) > 0) {
 	         max = c;                   
 	      }
+	      if(d.compareTo(max) > 0) {
+	    	  max = d;
+	      }
 	      return max;
 	   }
 	
 	public static void main(String[] args) {
-		genericBasedPrograms<Integer> B1 = new genericBasedPrograms<Integer>(1,2,3);
+		genericBasedPrograms<Integer> B1 = new genericBasedPrograms<Integer>(1,2,3,4);
 		System.out.println("The Maximum data is : " + B1.maxData());
 	}
 	
