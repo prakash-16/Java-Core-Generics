7ea8e59f29f4e0f1547ce0bdb9dab055bc2e9de9
Prakash-16 <goudprakash58@gmail.com>
2021-08-21 00:03:40 +0530

    [Prakash] - Refactored the code to one generic method

--- src/com/bridgelabz/JCG/genericBasedPrograms.java
+++ src/com/bridgelabz/JCG/genericBasedPrograms.java
@@ -1,32 +1,23 @@
 package com.bridgelabz.JCG;
 
 public class genericBasedPrograms {
-	public int i;
 	
 	
-	public String maxInteger(String a, String b, String c) {
-		if(a.compareTo(b) == 1) {
-			if(a.compareTo(c) == 1) {
-				return a;
-			}
-			else {
-				return c;
-			}
-		}
-		else {
-			if(b.compareTo(c) == 1) {
-				return b;
-			}
-			else {
-				return c;
-			}
-		}
-			
-	}
+	public static <E extends Comparable<E>> E maxData(E a, E b, E c) {
+	      E max = a;   
+	      
+	      if(b.compareTo(max) > 0) {
+	         max = b;   
+	      }
+	      
+	      if(c.compareTo(max) > 0) {
+	         max = c;                   
+	      }
+	      return max;     
+	   }
 	
 	public static void main(String[] args) {
-		genericBasedPrograms B1 = new genericBasedPrograms();
-		System.out.println("The maximum number in the list is  " + B1.maxInteger("Bananana","Apple","Peach"));
+		System.out.println(maxData("Apple","Banana","Peaches"));
 	}
 
 }
